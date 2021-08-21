2b954d17da1572db2974363f804102963b57e8b3
Ovenoboyo <ovenoboyo@gmail.com>
2021-08-21 09:29:49 +0530

    Bind router to all interfaces

--- v2/cmd/webserver/main.go
+++ v2/cmd/webserver/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/Ovenoboyo/basic_webserver/v2/pkg/handlers"
 
@@ -16,4 +18,13 @@ func main() {
 	r := mux.NewRouter()
 	http.Handle("/", r)
 	handlers.HandleLogin(r)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:80",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
